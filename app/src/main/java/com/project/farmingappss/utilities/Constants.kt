9eca589bbb221e9777befff3bd36f44db85ccf38
farhazulmullick <farhazul.mullick@pw.live>
2023-05-06 13:43:52 +0530

    Added CropDiseasePrediction Design

--- app/src/main/java/com/project/farmingappss/utilities/Constants.kt
+++ app/src/main/java/com/project/farmingappss/utilities/Constants.kt
@@ -4,6 +4,7 @@ const val YOJNA_URL = "yojna_url"
 
 // This type is used to Launch Fragment in MlBaseActivity
 const val ACTION_TYPE = "action_type"
+const val CAMERA_REQUEST = 101
 
 // Crop Disease Prediction
 const val CROP_DISEASE_PREDICTION_SCREEN = "crop_disease_prediction"
--- app/src/main/java/com/project/farmingappss/view/plantml/cropdiseaseprediction/CropDiseasePredictionFragment.kt
+++ app/src/main/java/com/project/farmingappss/view/plantml/cropdiseaseprediction/CropDiseasePredictionFragment.kt
@@ -1,11 +1,15 @@
 package com.project.farmingappss.view.plantml.cropdiseaseprediction
 
+import android.content.Intent
 import android.os.Bundle
+import android.provider.MediaStore
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import com.project.farmingappss.R
+import com.project.farmingappss.databinding.FragmentCropDiseasePredictionBinding
+import com.project.farmingappss.utilities.CAMERA_REQUEST
 
 /**
  * A simple [Fragment] subclass.
@@ -13,12 +17,19 @@ import com.project.farmingappss.R
  * create an instance of this fragment.
  */
 class CropDiseasePredictionFragment : Fragment() {
-    // TODO: Rename and change types of parameters
-    private var param1: String? = null
-    private var param2: String? = null
+
+    private var _binding: FragmentCropDiseasePredictionBinding? = null
+    private val binding get() = _binding!!
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+
+        activity?.run {
+            startActivityForResult(
+                Intent(MediaStore.ACTION_IMAGE_CAPTURE).apply {
+                }, CAMERA_REQUEST
+            )
+        }
     }
 
     override fun onCreateView(
@@ -26,24 +37,38 @@ class CropDiseasePredictionFragment : Fragment() {
         savedInstanceState: Bundle?
     ): View? {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_crop_disease_prediction, container, false)
+        _binding = FragmentCropDiseasePredictionBinding.inflate(layoutInflater)
+        return binding.root
     }
 
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+
+    }
+
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        super.onActivityResult(requestCode, resultCode, data)
+        when (requestCode) {
+            CAMERA_REQUEST -> {
+
+            }
+        }
+    }
+
+
+
     companion object {
-        /**
-         * Use this factory method to create a new instance of
-         * this fragment using the provided parameters.
-         *
-         * @param param1 Parameter 1.
-         * @param param2 Parameter 2.
-         * @return A new instance of fragment CropDiseasePredictionFragment.
-         */
-        // TODO: Rename and change types and number of parameters
         @JvmStatic
-        fun newInstance(param1: String="", param2: String="") =
+        fun newInstance() =
             CropDiseasePredictionFragment().apply {
                 arguments = Bundle().apply {
                 }
             }
+    }
+
+    override fun onDestroyView() {
+        _binding = null
+        super.onDestroyView()
     }
 }
